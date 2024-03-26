8e02d8571a00d3bca97d8e37088bf3d59adff9aa
Aitishechnik <145229205+Aitishechnik@users.noreply.github.com>
2024-03-26 12:28:30 +0300

    Create HomeWork1.js

---
+++ HomeWork1.js
@@ -0,0 +1,15 @@
+let articleRate = 3;
+if (articleRate === 5) {
+  console.log(`Rate is 5`);
+} else if (articleRate === 4) {
+  console.log(`Rate is 4`);
+} else if (articleRate === 3) {
+  console.log(`Rate is 3`);
+} else if (articleRate === 2) {
+  console.log(`Rate is 2`);
+} else if (articleRate === 1) {
+  console.log(`Rate is 1`);
+} else {
+  console.log(`Invalid value`);
+}
+//----------------------------
